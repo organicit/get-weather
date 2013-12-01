1b7db824e1b1493abf79431b608c2e84ed83d4fe
Eric Courville <organicit-git@outlook.com>
2013-11-30 23:27:33 -0800

    Updated Get-City Function

--- get-weather.psm1
+++ get-weather.psm1
@@ -80,107 +80,46 @@ function Get-City {
 	#>
 	[CmdletBinding()]
 	[OutputType([System.Int32])]
-	param(
-		[Parameter(Position=0, Mandatory=$true)]
-		[ValidateNotNullOrEmpty()]
-		[System.String]
-		$City,
-
-		[Parameter(Position=1)]
-		[ValidateNotNull()]
-		[System.String]
-		$CountryCode
-	)
-    $updName = $Name -replace " ","%20"
-    $url = $Global:apiRoot + "find?q=" + $updName
-    #Write-Host $url
-	try {
-		$LUVals = Invoke-RestMethod -Uri $url
-            
-        if($LUVals.count -eq 1)
-        {
-            
-            return $LUVals.list.id
-
-        } else {
-            Write-Host "More than one City has been returned:"
-            foreach($City in $LUVals)
-            {
-                Write-Host $City.List.Name
-                write-host $City.List.sys
-            }
-        }
-
-  	}
-	catch {
-		throw
-	}
-}
-
-function Set-City {
-	<#
-		.SYNOPSIS
-			Lookup city code
-
-		.DESCRIPTION
-			A detailed description of the function.
-
-		.PARAMETER  ParameterA
-			The description of the ParameterA parameter.
-
-		.PARAMETER  ParameterB
-			The description of the ParameterB parameter.
-
-		.EXAMPLE
-			PS C:\> Get-Something -ParameterA 'One value' -ParameterB 32
-
-		.EXAMPLE
-			PS C:\> Get-Something 'One value' 32
-
-		.INPUTS
-			System.String,System.Int32
-
-		.OUTPUTS
-			System.String
-
-		.NOTES
-			Additional information about the function go here.
-
-		.LINK
-			about_functions_advanced
-
-		.LINK
-			about_comment_based_help
-
-	#>
-	param(
-		[Parameter(Position=0, Mandatory=$true)]
-		[ValidateNotNullOrEmpty()]
-		[System.String]
-		$Name
-	)
+	param($CityName)
     
-    $updName = $Name -replace " ","%20"
-    $url = $Global:apiRoot + "find?q=" + $updName
-    #Write-Host $url
-	try {
-		$LUVals = Invoke-RestMethod -Uri $url
-            
-        if($LUVals.count -eq 1)
-        {
-            
-            return $LUVals.list.id
+    $CQuery = Invoke-RestMethod -uri "http://api.openweathermap.org/data/2.5/find?q=$CityName"
 
-        } else {
-            Write-Host "More than one City has been returned:"
-            foreach($City in $LUVals)
-            {
-                Write-Host $City.List.Name + ": " + $City.List.sys
-            }
-        }
+    #$a = @{}
 
-  	}
-	catch {
-		throw
-	}
+    $a = $CQuery.list|select id, coord
+
+    #$googs = Invoke-RestMethod -Uri "http://maps.googleapis.com/maps/api/geocode/json?latlng=38.661145,-121.182588&sensor=true"
+
+
+    #$returns = foreach($b in $a.coord) { $c = $b.lat; $d = $b.lon; $e = "http://maps.googleapis.com/maps/api/geocode/json?latlng=$c,$d&sensor=true"; Write-Host $e}
+    $array =@()
+    foreach($b in $a) { 
+            $c = $b.coord.lat
+            $d = $b.coord.lon
+            $f = $b.id
+            $e = (Invoke-RestMethod -uri "http://maps.googleapis.com/maps/api/geocode/json?latlng=$c,$d&sensor=true")
+            $state = $e.results.where({$_.types -eq "postal_code"}).address_components|where-object {$_.types -like "*1*"}|select long_name, short_name -First 1
+            $country = $e.results.where({$_.types -like "country*"}).address_components
+
+            $obj = New-Object System.Object 
+            $obj | Add-Member -membertype NoteProperty -Name State -value $state.long_name
+            $obj | Add-Member -MemberType NoteProperty -Name Country -Value $country.long_name
+            $obj | Add-Member -MemberType NoteProperty -Name CityCode -Value $f
+
+            $array += $obj
+        
+            #$e.results.where({$_.types -eq "postal_code"}).address_components.types
+
+
+            #$e.results.where({$_.types -eq "postal_code"}).address_components.where({$_.types.contains("country")}).long_name
+        
+            #$state = $e.results.where({$_.types -eq "postal_code"}).address_components.where({$_.types -like "administrative_area_level_1"}).long_name
+            #$country = $foo.results.where({$_.types -eq "postal_code"}).address_components.where({$_.types -like "country"}).long_name
+        
+            #write-host " select city id `{0} for state:{1} in country:{2}" -f $b.id,$state,$country
+            #Write-Host $b.id $state $country
+        
+            } 
+            Write-Output $array
+
 }
